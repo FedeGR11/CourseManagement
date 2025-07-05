af74c570ee25ac2d6c17eb2629ec3ae3728eeaf8
Federico Garcia <fedegr11@gmail.com>
2025-07-04 22:41:22 -0300

    Arreglo while(true) por do/while

--- src/Main.java
+++ src/Main.java
@@ -28,43 +28,50 @@ public class Main {
         System.out.println("Curso #" + number);
             
         String type;
-        while(true) {
-        System.out.print("Type: ");
-        type = scanner.nextLine().trim().replaceAll("\\s+", "").toLowerCase();
+        
+        do {
+            System.out.print("Type: ");
+            type = scanner.nextLine().trim().replaceAll("\\s+", "").toLowerCase();
             
+            if (!type.equals("online") && !type.equals("onsite") && !type.equals("")) {
+            System.out.println("Tipo inválido. Intente con 'online', 'onsite' o deje vacío para indefinido.");
+        }
+
+        } while (!type.equals("online") && !type.equals("onsite") && !type.equals(""));
+
+
         System.out.print("Title: ");
         String title = scanner.nextLine();
                 
         System.out.print("Duration(hs): ");
         int duration = positiveNumber(scanner);
-
         scanner.nextLine();
 
         System.out.print("Professor: ");
         String professor = scanner.nextLine();
                 
-            if (type.equals("online")) {
-                System.out.print("Plataform: ");
-                String plataform = scanner.nextLine();
-                System.out.println(" "); 
-                return new OnlineCourse(title, duration, professor, plataform);
+        if (type.equals("online")) {
+            System.out.print("Plataform: ");
+            String plataform = scanner.nextLine();
+            System.out.println(" "); 
+            return new OnlineCourse(title, duration, professor, plataform);
 
-            } else if (type.equals("onsite")) {
-                System.out.print("Room: ");
-                int room = scanner.nextInt();
+        } else if (type.equals("onsite")) {
+            System.out.print("Room: ");
+            int room = scanner.nextInt();
 
-                System.out.print("Quota: ");
-                int maxQuota = positiveNumber(scanner);
-                scanner.nextLine();
-                System.out.println(" "); 
-                return new OnSiteCourse(title, duration, professor, room, maxQuota);
+            System.out.print("Quota: ");
+            int maxQuota = positiveNumber(scanner);
+            scanner.nextLine();
+            System.out.println(" "); 
+            return new OnSiteCourse(title, duration, professor, room, maxQuota);
                  
-            } else {
-                System.out.println(" "); 
-                return new Course("[Undefined] " + title, duration, professor);
-            }
+        } else {
+            System.out.println(" "); 
+            return new Course("[Undefined] " + title, duration, professor);
         }
-    }  
+    }   
+     
     public static void showCourses(Course[] courses) {
         System.out.println("--- Información de cursos registrados ---");
         for (Course course : courses) {
@@ -74,19 +81,20 @@ public class Main {
     public static int positiveNumber(Scanner scanner) {
         int number;
 
-        while (true) {
-            if (scanner.hasNextInt()) {
-                number = scanner.nextInt();
-
-                if (number > 0) {
-                    return number;
-                } else {
-                    System.out.println("Error: Debes ingresar un número positivo.");
-                }
-            } else {
+        do { 
+            while (!scanner.hasNextInt()) {
                 System.out.println("Error: Entrada inválida. Por favor, ingresa un número.");
-               scanner.next();
+                scanner.next();
             }
-        }
+            
+            number = scanner.nextInt();
+
+            if (number <= 0) {
+                System.out.println("Error: Debes ingresar un número positivo.");
+            }   
+            
+        } while (number <= 0);
+
+        return number;
     }
 }
